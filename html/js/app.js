595d17f28f99573e98e6d0c134e18b411586c18d
JuanManuelPiedrahita <jmpiedrahita31@misena.edu.co>
2019-03-27 14:28:17 -0500

    Programas

--- html/js/app.js
+++ html/js/app.js
@@ -1,3 +1,3 @@
 var nombre = document.getElementById('nombre');
-var pregunta = prompt('Cual es su nombre?');
-nombre.innerHTML =  pregunta;
+var respuesta = prompt('Cuál es su nombre ?');
+nombre.innerHTML = respuesta;
